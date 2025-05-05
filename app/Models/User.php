8f6dab4d9d7045706f7d86313dfacff22fa9fb17
Strugariu Sergiu <sstrugariu.7@icloud.com>
2025-05-06 00:01:30 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,6 +73,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@twodevs.ro') && $this->isAdmin();
+        return str_ends_with($this->email, '@twodevs.ro');
     }
 }
